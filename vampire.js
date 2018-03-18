6987bd5f27a5353d58be5b86a3df07cc4ccf3222
Zharphyn <brad@zharphyn.ca>
2018-03-18 21:00:36 +0000

    in progress

--- vampire.js
+++ vampire.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,12 +12,13 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+   return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
