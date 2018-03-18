ab15ed96e13d51da1150a2d0ad9baa93ff0fab16
Zharphyn <brad@zharphyn.ca>
2018-03-18 21:02:46 +0000

    Added more senior than

--- vampire.js
+++ vampire.js
@@ -35,7 +35,8 @@ class Vampire {
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal) return true;
+    return false;
   }
 
   /** Stretch **/
